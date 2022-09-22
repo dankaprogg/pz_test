4578cf79f627a2af5b99b5f15b423a9b7e3c1532
Daniil Shvedov <dankaprog@gmail.com>
2022-09-23 01:19:21 +0300

    Add foo_recursive.py

---
+++ foo_recursive.py
@@ -0,0 +1,36 @@
+from sqlalchemy import literal
+from sqlalchemy import null
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Query
+from sqlalchemy.orm import aliased
+
+from db.models import Foo
+
+
+async def foo_recursive(session: AsyncSession, depth: int):
+    hierarchy = (
+        Query(Foo)
+            .add_columns(literal(0).label('level'))
+            .filter(Foo.parent_id == null())
+            .cte(name="hierarchy", recursive=True)
+    )
+
+    parent = aliased(hierarchy, name="p")
+    children = aliased(Foo, name="c")
+    hierarchy = hierarchy.union_all(
+        Query(children).add_columns((parent.c.level + 1).label("level"))
+            .filter(children.parent_id == parent.c.id)
+            .filter(parent.c.level < depth)
+    )
+
+    stmt = Query(
+        Foo,
+        hierarchy.c.level
+    ).select_entity_from(hierarchy)
+
+    async with session as transaction:
+        result = await transaction.execute(stmt)
+
+    result = result.all()
+
+    return result
