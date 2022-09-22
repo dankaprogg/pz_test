add32ae54c1fb3a106c7f8a861a50cfcc3b8d4c8
Daniil Shvedov <dankaprog@gmail.com>
2022-09-23 02:02:06 +0300

    Add sort_dir to foo_recursive Fix & append typing of foo_recursive

--- foo_recursive.py
+++ foo_recursive.py
@@ -1,9 +1,11 @@
+from typing import List
+
 from sqlalchemy import literal
 from sqlalchemy import null
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import InstrumentedAttribute
 from sqlalchemy.orm import Query
 from sqlalchemy.orm import aliased
-from sqlalchemy.sql.schema import Column
 
 from db.models import Foo
 
@@ -11,9 +13,25 @@ from db.models import Foo
 async def foo_recursive(
     session: AsyncSession,
     depth: int,
-    sort_fld: Column,
+    sort_fld: InstrumentedAttribute,
+    sort_dir: str,
     root_id: str = None,
-):
+) -> List[Foo]:
+    """
+
+    :param session: AsyncSession
+    :param depth: int, depth of recursive select
+    :param sort_fld: sort field
+    :param sort_dir: sort direction, "asc" or "desc"
+    :param root_id: optional, id of root element, if not presented, any element with parent_id=null will be used
+    :return:
+    """
+    if sort_dir == "asc":
+        sort_fld = sort_fld.asc()
+    elif sort_dir == "desc":
+        sort_fld = sort_fld.desc()
+    else:
+        raise Exception("Wrong sort_dir")
     hierarchy = Query(Foo).add_columns(literal(0).label('level'))
 
     if root_id:
